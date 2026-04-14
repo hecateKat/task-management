a89e82d4b138b1f537d4a7a92c2a152c5513d9e2
Kat <kat.ochmanska@gmail.com>
2026-04-14 15:11:08 +0200

    initial commit

---
+++ src/main/java/com/kat/taskmanagement/TaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kat.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TaskManagementApplication.class, args);
+    }
+
+}
