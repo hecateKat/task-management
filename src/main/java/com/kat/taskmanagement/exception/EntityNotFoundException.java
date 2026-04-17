2d09a314fbff6b8efe857c5abc7b88041fd1051e
Kat <kat.ochmanska@gmail.com>
2026-04-17 09:56:30 +0200

    security

---
+++ src/main/java/com/kat/taskmanagement/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.kat.taskmanagement.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
+
